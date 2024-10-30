69d8934ea8012a4bf1f2c78a6db717b2189f5d4c
khm6211 <khm6211@gmail.com>
2024-10-30 14:38:28 +0900

    OAuth 테스트, 디버그뷰어 추가

---
+++ Assets/Assets/Script/HM/Login.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    public string Token;
+    public string Error;
+
+    void Awake()
+    {
+        //Initialize PlayGamesPlatform
+        PlayGamesPlatform.Activate();
+        LoginGooglePlayGames();
+    }
+
+    public void LoginGooglePlayGames()
+    {
+        PlayGamesPlatform.Instance.Authenticate((success) =>
+        {
+            if (success == SignInStatus.Success)
+            {
+                Debug.Log("Login with Google Play games successful.");
+
+                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+                {
+                    Debug.Log("Authorization code: " + code);
+                    Token = code;
+// This token serves as an example to be used for SignInWithGooglePlayGames
+                });
+            }
+            else
+            {
+                Error = "Failed to retrieve Google play games authorization code";
+                Debug.Log("Login Unsuccessful");
+            }
+        });
+    }
+
+    public void LogOutGooglePlayGames()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+    }
+}
